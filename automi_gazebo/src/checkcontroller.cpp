4ee6cc8ce317bcb835e2600e116c2efecf06b849
Arunim Joarder <reevu123@gmail.com>
2020-06-15 20:58:26 +0530

    fixd node

--- automi_gazebo/src/checkcontroller.cpp
+++ automi_gazebo/src/checkcontroller.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-    ros::init(argc,argv,"chheckcontroller");
+    ros::init(argc,argv,"checkcontroller");
     ros::NodeHandle nh;
     ros::Publisher pub = nh.advertise<trajectory_msgs::JointTrajectory>("/automi/master_controller/command",10);
     ros::Rate loopRate(10);
@@ -12,13 +12,20 @@ int main(int argc, char** argv)
     {
         // ros::spinOnce();
         trajectory_msgs::JointTrajectory ans;
+        trajectory_msgs::JointTrajectoryPoint point;
+        
         ans.joint_names.push_back("r_knee"); 
         ans.joint_names.push_back("l_elbow");
-        ans.points[0].positions.resize(2);
-        ans.points[0].positions[0] = 2;
-        ans.points[0].positions[1] = 5;
-        ans.points[0].time_from_start.sec = 2;
+        
+        point.positions.push_back(1.57);
+        point.positions.push_back(3.14);
+        point.time_from_start.sec = 2;
+        ans.points.push_back(point);
+        
         ans.header.stamp = ros::Time::now();
+        
         pub.publish(ans);
+        
+        loopRate.sleep();
     }
 }
